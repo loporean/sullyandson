3fe32b3c024001d68d8f33efca3587ed8c1b5730
Spencer Denney <84533280+loporean@users.noreply.github.com>
2025-07-08 08:58:16 -0700

    Update quote.php

--- forms/quote.php
+++ forms/quote.php
@@ -29,8 +29,8 @@
   
   $contact->smtp = array(
     'host' => 'mail.smtp2go.com',
-    'username' => 'sullyandson.com',
-    'password' => 'IGdUzCnl4ioRxzIL',
+    'username' => '',
+    'password' => '',
     'port' => '2525'
   );
   
